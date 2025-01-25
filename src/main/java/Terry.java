11e166c443a6a96fb1b7d1503de6f7c1021a5e27
Yikbing <E1156223@u.nus.edu>
2025-01-25 12:50:10 +0800

    rename duke to terry, add greeting and exit message

---
+++ src/main/java/Terry.java
@@ -0,0 +1,16 @@
+public class Terry {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from Terry\n");
+
+        System.out.println("Hello! I am Terrybear\n ");
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------");
+        System.out.println("BYEBYE SEE YOU NEXT TIME\n");
+    }
+}
